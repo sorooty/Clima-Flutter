9fcf072b3a8d9694161b9c9da69da352505d95e0
sorooty <senseybalde@gmail.com>
2025-04-28 13:00:39 +0000

    Made advancements on the project-app : geolocator library discovery, creation of a location class to fetch current user location data / informations, started to implement the Weather API.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import "../services/location.dart";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +8,61 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // final LocationSettings locationSettings = LocationSettings(
+  //   accuracy: LocationAccuracy.bestForNavigation,
+  //   distanceFilter: 100,
+  // );
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location Loc = Location();
+    await Loc.getCurrentLocation();
+    print(Loc.latitude);
+    print(Loc.longitude);
+
+    // try {
+    //   Position position = await Geolocator.getCurrentPosition(
+    //       locationSettings: locationSettings);
+    //   print(position);
+    // } catch (e) {
+    //   print(e);
+    // }
+  }
+
+// To discover the "throw" command :
+  // smthThatExpectsLessThan10(12);
+  // void smthThatExpectsLessThan10(int n) {
+  //   if (n > 10) {
+  //     throw 'n is greater than 10, n should always be less than 10';
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   String myMargin = "abc";
+  //   double myMarginAsDouble = 0;
+
+  //   try {
+  //     myMarginAsDouble = double.parse(myMargin);
+  //   } catch (e) {
+  //     print("Should be a number not a string !");
+  //   }
+
+  //   return Scaffold(
+  //       body: Container(
+  //     margin: EdgeInsets.all(30.0),
+  //     // margin: EdgeInsets.all(myMarginAsDouble ?? 30) <=> "If the value of the variable is null => we assign a default value (30 in this case)".
+  //     color: Colors.red,
+  //   ));
+  // }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,23 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.low,
+    distanceFilter: 100,
+  );
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+      latitude = position.latitude;
+      longitude = position.longitude;
+      // print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
